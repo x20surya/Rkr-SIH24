20a6330065f8ba52b3673f77bc4c779a27223b60
Aryanchoudhary12 <aryankumar911315@gmail.com>
2024-09-04 23:42:39 +0530

    commited

--- client-web/src/_auth/forms/SignupForm.jsx
+++ client-web/src/_auth/forms/SignupForm.jsx
@@ -1,5 +1,12 @@
+import {useForm} from "react-hook-form"
+import { Form,FormControl,FormDescription,FormField,FormItem,FormLabel,FormMessage} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+
 export default function SignUpForm() {
+
     return(
-        <h1>Sign up</h1>
+        <div>
+            
+        </div>
     )
 }
