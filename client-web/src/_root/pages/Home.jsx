5a5251a7b7e7eabf91544e4a96f07b2c67567047
x20surya <suryanshuc659@gmail.com>
2024-09-06 12:11:54 +0530

    Update Homepage and auth pages

--- client-web/src/_root/pages/Home.jsx
+++ client-web/src/_root/pages/Home.jsx
@@ -1,15 +1,54 @@
 import { Button } from "@/components/ui/button";
 import { borderblue, yellowcloud1 } from "@/constants/borders";
+import { Link } from "react-router-dom";
+import Autoplay from "embla-carousel-autoplay";
+import { useRef } from "react";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function Home() {
   const borderurl = borderblue;
+  const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
   return (
-    <>
-      <div
+    <div className="w-full h-full flex justify-center bg-background">
+      <div className="w-3/4 pt-14 flex flex-col items-end gap-5">
+        <Carousel
+          plugins={[plugin.current]}
+          className="w-full"
+          onMouseEnter={plugin.current.stop}
+          onMouseLeave={plugin.current.reset}
+        >
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-[16/6] items-center justify-center p-6">
+                      <span className="text-4xl font-semibold">
+                        {index + 1}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+        <Button className="border-[4px] rounded-none border-card-foreground">Shop now</Button>
+      </div>
+      {/* <div
         style={{ borderImage: `url(.${borderurl}) 33% round` }}
         className={`w-[50%] h-[50%] border-[50px]`}
-      ></div>
-      <img className="h-10" src={yellowcloud1} />
-    </>
+      ></div> */}
+    </div>
   );
 }
