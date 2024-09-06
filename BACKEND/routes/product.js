68e326a4b1f9457ec9e6f7fdda43116a181bcbe4
Aakrisht Srivastava <aakrisri55@gmail.com>
2024-09-07 01:28:26 +0530

    add editProduct

--- BACKEND/routes/product.js
+++ BACKEND/routes/product.js
@@ -3,6 +3,8 @@ import { JWT_seller_authentication } from "../middleware/seller.js";
 import { Product } from "../db/product.js";
 import { Seller } from "../db/seller.js";
 import jwt from "jsonwebtoken"
+import { userJWTAuthentication } from "../middleware/user.js";
+import { productExists, sellerHasProduct } from "../middleware/product.js";
 
 const router = Router()
 
@@ -18,6 +20,22 @@ router.post("/add",JWT_seller_authentication ,async (req, res) => {
     const price = req.body.price
     const description = req.body.description
     const tags = req.body.tags
+
+    if(!name || !description || !stock || !price || !dimensions){
+        return res.json({
+            error : "Necessary details not maintained"
+        })
+    }   
+    if(tags.length() < 3){
+        return res.json({
+            error : "Insufficient Tags"
+        })
+    }
+    if(price <= 0){
+        return res.json({
+            error : "Invalid Price"
+        })
+    }
 
     const product = new Product({
         stock,
@@ -37,17 +55,56 @@ router.post("/add",JWT_seller_authentication ,async (req, res) => {
     seller.save()
 
     return res.json({
-        msg : "Product Added"
+        msg : "Product Added",
+        productId : product.id,
+        sellerId,
+        name
     })
 })
 
-router.get("/getProduct", (req, res) => {
+router.get("/getProduct",userJWTAuthentication , (req, res) => {
     const pid = req.body.productId
     const product = new Product.findById(pid)
     return product
 })
 
-router.get("/getProductByFilter", (req, res) => {
+router.post("/editProduct",JWT_seller_authentication ,productExists ,sellerHasProduct, async (req, res) => {
+    const pid = req.body.product
+
+    const stock = req.body.stock
+    const dimensions = req.body.dimensions
+    const name = req.body.name
+    const price = req.body.price
+    const description = req.body.description
+    const tags = req.body.tags
+
+    const product = await Product.findById(pid)
+
+    if(stock){
+        product.stock = stock
+    }
+    if(dimensions){
+        product.dimensions = dimensions
+    }
+    if(name){
+        product.name = name
+    }
+    if(price){
+        product.price = price
+    }
+    if(description){
+        product.descripton = description
+    }
+    if(tags){
+        product.tags = tags
+    }
+    
+    product.save()
+
+    return res.json(product)
+})
+
+router.get("/getProductByFilter", userJWTAuthentication, (req, res) => {
     const temp_filter = req.body.filter
     const filter = JSON.parse(temp_filter)
 
